ce70cbe14402f47dd5f6ba44d62d5e5b05371674
JanistaSrilerjan <janista_sr@cmu.ac.th>
2017-09-21 23:12:56 +0700

    test custom font

--- app/src/main/java/com/example/jnstar/urqproject/MainActivity.java
+++ app/src/main/java/com/example/jnstar/urqproject/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jnstar.urqproject;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +10,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_specify_q;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        tv_specify_q=(TextView)findViewById(R.id.tv_specify_q);
+        Typeface tf_1=Typeface.createFromAsset(getAssets(),"fonts/CmPrasanmit.ttf");
+        tv_specify_q.setTypeface(tf_1);
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
