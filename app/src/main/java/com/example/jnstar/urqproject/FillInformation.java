7361d47365111c1737aea1e19c6baf9b4b351067
JanistaSrilerjan <janista_sr@cmu.ac.th>
2017-09-22 01:22:05 +0700

    add fill information XML and edit content_main

---
+++ app/src/main/java/com/example/jnstar/urqproject/FillInformation.java
@@ -0,0 +1,22 @@
+package com.example.jnstar.urqproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class FillInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_information);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
