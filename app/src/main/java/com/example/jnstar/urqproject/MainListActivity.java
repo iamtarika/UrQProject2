88313289656138f80ff8d158bec12fb4f1cb9f7c
JanistaSrilerjan <janista_sr@cmu.ac.th>
2017-12-07 21:27:37 +0700

    add search.png and edit menu bar

---
+++ app/src/main/java/com/example/jnstar/urqproject/MainListActivity.java
@@ -0,0 +1,27 @@
+package com.example.jnstar.urqproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
